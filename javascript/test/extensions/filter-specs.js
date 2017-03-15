faebbe4cf1049aaa78ecedd12d5f72c2f27a6f53
Dan Gilkerson <contact@dangilkerson.com>
2017-03-14 20:14:08 -0700

    Filter specs

--- javascript/test/extensions/filter-specs.js
+++ javascript/test/extensions/filter-specs.js
@@ -39,4 +39,58 @@ describe('Extensions: filters', () => {
 
         return glanceSelector('item#every-other').should.deep.equal(dom.get('target-1', 'target-2'));
     });
+
+    it('should filter items for option', () => {
+        dom.render(
+            <div>
+                <div id="target-1">1</div>
+                <div id="target-2">12</div>
+                <div>123</div>
+                <div>1234</div>
+            </div>
+        );
+
+        glanceSelector.addExtension({
+            options: {
+                'lessthan3characters': function ({elements}) {
+                    return elements.filter(e => e.innerHTML.length < 3);
+                }
+            }
+        });
+
+        return glanceSelector('1#lessthan3characters').should.deep.equal(dom.get('target-1', 'target-2'));
+    });
+
+    it('should support setting the locator', () => {
+        dom.render(
+            <div>
+                <div>item</div>
+                <div id="target">something</div>
+            </div>
+        );
+
+        glanceSelector.addExtension({
+            options: {
+                'exact-match': {
+                    locate: function ({label, containerElements}) {
+                        return [dom.get('target')];
+                    }
+                }
+            }
+        });
+
+        return glanceSelector('something custom#exact-match').should.deep.equal(dom.get('target'));
+    });
+
+    it("should still use default filters if specified options don't have filters", function () {
+        dom.render(<span id="target"></span>)
+
+        glanceSelector.addExtension({
+            options: {
+                "optionwithoutfilter": {}
+            }
+        });
+
+        return glanceSelector("span#optionwithoutfilter").should.deep.equal(dom.get('target'));
+    });
 });
