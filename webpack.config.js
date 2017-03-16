afb363bd3a2c089ff29ec95473de27f6e3d9e4b7
Dan Gilkerson <contact@dangilkerson.com>
2017-03-15 20:56:03 -0700

    Build and deploy browser script

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: './javascript/src/browser/index.js',
     output: {
         filename: './dist/glance-selector.js'
     },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            mangle: false
+        })
+    ],
     module: {
         loaders: [
             {
