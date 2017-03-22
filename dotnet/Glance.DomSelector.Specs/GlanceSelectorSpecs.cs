5a169c86ead520d9e22842445795d8e4a24cbb41
Dan Gilkerson <contact@dangilkerson.com>
2017-03-22 04:52:00 -0700

    Initial dotnet package

---
+++ dotnet/Glance.DomSelector.Specs/GlanceSelectorSpecs.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+
+namespace Glance.DomSelector.Specs
+{
+    [TestFixture]
+    public class GlanceSelectorSpecs
+    {
+        [Test]
+        public void ShouldFindOneElement()
+        {
+            using (var driver = new RemoteWebDriver(DesiredCapabilities.Chrome()))
+            {
+                driver.Navigate().GoToUrl("http://quasimatic.org/take-a-glance/?level=2");
+
+                var glance = new GlanceSelector();
+
+                var element = driver.FindElement(glance.Locate("square"));
+
+                Assert.NotNull(element);
+            }
+        }
+
+        [Test]
+        public void ShouldFindMultipleElements()
+        {
+            using (var driver = new RemoteWebDriver(DesiredCapabilities.Chrome()))
+            {
+                driver.Navigate().GoToUrl("http://quasimatic.org/take-a-glance/?level=6");
+
+                var glance = new GlanceSelector();
+
+                var elements = driver.FindElements(glance.Locate("square"));
+
+                Assert.AreEqual(elements.Count, 3);
+            }
+        }
+    }
+}
---
+++ dotnet/Glance.DomSelector/GlanceSelector.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace Glance.DomSelector
+{
+    public class GlanceSelector
+    {
+        private class GlanceLocator : By
+        {
+            private string reference;
+
+            private void loadGlance(IJavaScriptExecutor driver)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var stream = assembly.GetManifestResourceStream("Glance.DomSelector.glance-selector.js");
+                var glanceSelectorScript = new StreamReader(stream).ReadToEnd();
+                driver.ExecuteScript(glanceSelectorScript);
+            }
+
+            public GlanceLocator(string reference)
+            {
+                this.reference = reference;
+            }
+
+            public override IWebElement FindElement(ISearchContext context)
+            {
+                var driver = (RemoteWebDriver) context;
+                loadGlance(driver);
+                string executeGlance = "return glanceSelector(arguments[0]);";
+                var element = driver.ExecuteScript(executeGlance, this.reference);
+                return element as IWebElement;
+            }
+
+            public override ReadOnlyCollection<IWebElement> FindElements(ISearchContext context)
+            {
+                var driver = (RemoteWebDriver) context;
+                loadGlance(driver);
+                string executeGlance = "return glanceSelector(arguments[0]);";
+                var elements = driver.ExecuteScript(executeGlance, reference) as ReadOnlyCollection<IWebElement>;
+                return elements;
+            }
+        }
+
+        public By Locate(string reference)
+        {
+            return new GlanceLocator(reference);
+        }
+    }
+}
