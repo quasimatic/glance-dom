cc930cb0ba0dc2bc3eb483aacc94946ace84e231
Dan Gilkerson <contact@dangilkerson.com>
2017-04-14 20:41:32 -0700

    Ignore default locators if a locator is specified

--- javascript/src/command-queue/locator-preprocessor-spec.js
+++ javascript/src/command-queue/locator-preprocessor-spec.js
@@ -2,106 +2,141 @@ import Extensions from '../extensions';
 import Preprocessor from './locator-preprocessor';
 
 let extensions = new Extensions([{
-    options: {
-        'default-locator-1': {
-            locate: 'custom'
-        }
-    }
+	options: {
+		'default-locator-1': {
+			locate: 'custom'
+		},
+		'default-locator-2': {
+			locate: 'custom'
+		}
+	}
 }]);
 
-let defaultOptions = ['default-locator-1'];
+let defaultOptions = ['default-locator-1', 'default-locator-2'];
 
 describe('Preprocessor: locators', () => {
-    it('should support no options', () => {
-        let preprocessor = new Preprocessor();
-        preprocessor.extensions.extensions.length.should.equal(0);
-        preprocessor.defaultOptions.length.should.equal(0);
-    });
+	it('should support no options', () => {
+		let preprocessor = new Preprocessor();
+		preprocessor.extensions.extensions.length.should.equal(0);
+		preprocessor.defaultOptions.length.should.equal(0);
+	});
 
-    it('should have no default extensions or options', () => {
-        let preprocessor = new Preprocessor({});
-        preprocessor.extensions.extensions.length.should.equal(0);
-        preprocessor.defaultOptions.length.should.equal(0);
-    });
+	it('should have no default extensions or options', () => {
+		let preprocessor = new Preprocessor({});
+		preprocessor.extensions.extensions.length.should.equal(0);
+		preprocessor.defaultOptions.length.should.equal(0);
+	});
 
-    it('should apply default locators', () => {
-        let preprocessor = new Preprocessor({extensions, defaultOptions});
+	it('should apply default locators', () => {
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
 
-        let commands = preprocessor.getLocatorCommands({label: 'subject', options: []});
+		let commands = preprocessor.getLocatorCommands({label: 'subject', options: []});
 
-        commands.should.deep.equal([{command: 'locate', label: 'subject', option: 'default-locator-1'}]);
-    });
+		commands.should.deep.equal([
+			{command: 'locate', label: 'subject', option: 'default-locator-1'},
+			{command: 'locate', label: 'subject', option: 'default-locator-2'}
+		]);
+	});
 
-    it('should not apply option without locator', () => {
-        let customExtension = {
-            options: {
-                'custom-filter': () => {
-                }
-            }
-        };
+	it('should not apply option without locator', () => {
+		let customExtension = {
+			options: {
+				'custom-filter': () => {
+				}
+			}
+		};
 
-        extensions.add(customExtension);
+		extensions.add(customExtension);
 
-        let preprocessor = new Preprocessor({extensions, defaultOptions});
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
 
-        let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['custom-locator']});
+		let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['custom-locator']});
 
-        commands.should.deep.equal([{command: 'locate', label: 'subject', option: 'default-locator-1'}]);
-    });
+		commands.should.deep.equal([
+			{command: 'locate', label: 'subject', option: 'default-locator-1'},
+			{command: 'locate', label: 'subject', option: 'default-locator-2'}
+		]);
+	});
 
-    it('should apply additional locator by option', () => {
-        let customExtension = {
-            options: {
-                'custom-locator': {
-                    locate: 'custom'
-                }
-            }
-        };
+	it('should apply additional locator by option', () => {
+		let customExtension = {
+			options: {
+				'custom-locator': {
+					locate: 'custom'
+				}
+			}
+		};
 
-        extensions.add(customExtension);
+		extensions.add(customExtension);
 
-        let preprocessor = new Preprocessor({extensions, defaultOptions});
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
 
-        let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['custom-locator']});
+		let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['custom-locator']});
 
-        commands.should.deep.equal([
-            {command: 'locate', label: 'subject', option: 'default-locator-1'},
-            {command: 'locate', label: 'subject', option: 'custom-locator'}
-        ]);
-    });
+		commands.should.deep.equal([
+			{command: 'locate', label: 'subject', option: 'custom-locator'}
+		]);
+	});
 
-    it('should apply custom label locator with defaults', () => {
-        extensions.add({
-            labels: {
-                'custom-label': 'custom'
-            }
-        });
+	it('should apply custom label locator with defaults', () => {
+		extensions.add({
+			labels: {
+				'custom-label': 'custom'
+			}
+		});
 
-        let preprocessor = new Preprocessor({extensions, defaultOptions});
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
 
-        let commands = preprocessor.getLocatorCommands({label: 'custom-label', options: []});
+		let commands = preprocessor.getLocatorCommands({label: 'custom-label', options: []});
 
-        commands.should.deep.equal([
-            {command: 'locate', label: 'custom-label', option: 'custom-label'},
-            {command: 'locate', label: 'custom-label', option: 'default-locator-1'}]);
-    });
+		commands.should.deep.equal([
+			{command: 'locate', label: 'custom-label', option: 'custom-label'},
+			{command: 'locate', label: 'custom-label', option: 'default-locator-1'},
+			{command: 'locate', label: 'custom-label', option: 'default-locator-2'}
+		]);
+	});
 
-    it('should apply dynamic locator with defaults', () => {
-        extensions.add({
-            locator: {
-                check: () => true,
-                locate: () => {
-                }
-            }
-        });
+	it('should apply dynamic locators', () => {
+		extensions.add({
+			locator: {
+				check: () => true,
+				locate: () => {
+				}
+			}
+		});
 
-        let preprocessor = new Preprocessor({extensions, defaultOptions});
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
 
-        let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['dynamic']});
+		let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['dynamic']});
 
-        commands.should.deep.equal([
-            {command: 'locate', label: 'subject', option: 'default-locator-1'},
-            {command: 'locate', label: 'subject', option: 'dynamic'}
-        ]);
-    });
+		commands.should.deep.equal([{command: 'locate', label: 'subject', option: 'dynamic'}]);
+	});
+
+	it('should ignore defaults if option with a locator is specified', () => {
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
+
+		let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['default-locator-1']});
+
+		commands.should.deep.equal([
+			{command: 'locate', label: 'subject', option: 'default-locator-1'}
+		]);
+	});
+
+	it('should not ignore default locators if a specified option is only a filter', () => {
+		extensions.add({
+			options: {
+				'filter': () => {
+				}
+			}
+		});
+
+		let preprocessor = new Preprocessor({extensions, defaultOptions});
+
+		let commands = preprocessor.getLocatorCommands({label: 'subject', options: ['filter']});
+
+		commands.should.deep.equal([
+			{command: 'locate', label: 'subject', option: 'default-locator-1'},
+			{command: 'locate', label: 'subject', option: 'default-locator-2'}
+		]);
+	});
 });
--- javascript/src/command-queue/locator-preprocessor.js
+++ javascript/src/command-queue/locator-preprocessor.js
@@ -1,67 +1,74 @@
 import Extensions from '../extensions';
 
 export default class LocatorPreprocessor {
-    constructor({extensions = new Extensions(), defaultOptions = []} = {
-                    extensions: new Extensions(),
-                    defaultOptions: []
-                }) {
-        this.extensions = extensions;
-        this.defaultOptions = defaultOptions;
-    }
+	constructor({extensions = new Extensions(), defaultOptions = []} = {
+		extensions: new Extensions(),
+		defaultOptions: []
+	}) {
+		this.extensions = extensions;
+		this.defaultOptions = defaultOptions;
+	}
 
-    getLocatorCommands(target) {
-        let extensions = this.extensions;
-        let locators = [];
-        let labels = extensions.getLabels();
+	getLocatorCommands(target) {
+		let extensions = this.extensions;
+		let locators = [];
+		let labels = extensions.getLabels();
 
-        if ((typeof(labels[target.label]) === 'object' && labels[target.label].locate) || labels[target.label]) {
-            locators = [{command: 'locate', option: 'custom-label', label: target.label}];
-        }
+		if ((typeof(labels[target.label]) === 'object' && labels[target.label].locate) || labels[target.label]) {
+			locators = [{command: 'locate', option: 'custom-label', label: target.label}];
+		}
 
-        return this.locatorsFromOptions(target, locators, extensions);
-    }
+		return this.locatorsFromOptions(target, locators, extensions);
+	}
 
-    locatorsFromOptions(target, locators, extensions) {
-        let updatedOptionTarget = this.configureOptionsWithLocators(target);
+	locatorsFromOptions(target, locators, extensions) {
+		let updatedOptionTarget = this.configureOptionsWithLocators(target);
 
-        updatedOptionTarget.options.forEach(name => locators.push({
-            command: 'locate',
-            option: name,
-            label: target.label
-        }));
+		updatedOptionTarget.options.forEach(name => locators.push({
+			command: 'locate',
+			option: name,
+			label: target.label
+		}));
 
-        return locators;
-    }
+		return locators;
+	}
 
-    configureOptionsWithLocators(target) {
-        let extensions = this.extensions;
-        let defaultOptions = this.defaultOptions;
-        let options = extensions.getOptions();
-        let possibleOptions = target.options;
-        let validOptions = [];
+	configureOptionsWithLocators(target) {
+		let defaultOptions = this.defaultOptions;
+		let possibleOptions = target.options;
 
-        if (defaultOptions.length > 0)
-            possibleOptions = defaultOptions.concat(possibleOptions);
+		let validOptions = this.optionsWithLocators(possibleOptions, target.label);
 
-        possibleOptions.forEach(name => {
-            if (options[name] && options[name].locate) {
-                validOptions = validOptions.concat(name);
-            }
-            else {
-                let catchAlls = extensions.getExtensions().filter(e => {
-                    if (e.locator) {
-                        return e.locator.check({label: target.label, option: name});
-                    }
+		if (validOptions.length === 0 && defaultOptions.length > 0)
+			validOptions = this.optionsWithLocators(defaultOptions, target.label);
 
-                    return false;
-                });
+		return {...target, options: validOptions};
+	}
 
-                if (catchAlls.length > 0) {
-                    validOptions = validOptions.concat(catchAlls.map(e => (name)));
-                }
-            }
-        });
+	optionsWithLocators(possibleOptions, label) {
+		let validOptions = [];
+		let options = this.extensions.getOptions();
+		let extensions = this.extensions;
 
-        return {...target, options: validOptions};
-    }
+		possibleOptions.forEach(name => {
+			if (options[name] && options[name].locate) {
+				validOptions = validOptions.concat(name);
+			}
+			else {
+				let catchAlls = extensions.getExtensions().filter(e => {
+					if (e.locator) {
+						return e.locator.check({label, option: name});
+					}
+
+					return false;
+				});
+
+				if (catchAlls.length > 0) {
+					validOptions = validOptions.concat(catchAlls.map(e => (name)));
+				}
+			}
+		});
+
+		return validOptions;
+	}
 };
