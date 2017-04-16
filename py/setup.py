6026e61e3bb6c6e638cacfc5230870ad1bafd5e4
Jim Brännlund <jimbrannlund@fastmail.com>
2017-04-16 23:29:56 +0200

    Put some meta in setup.py

---
+++ py/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='glance_dom',
+    packages=['glance_dom'],
+    package_data={
+        'glance_dom': ['scr/glance-dom.js'],
+    },
+    version='0.1',
+    description='A selenium automation tool that utilizes contextual labels to automate faster and loosen the ties to '
+                'the DOM.',
+    author='Dan Gilkerson',
+    author_email='info@quasimatic.org',
+    url='https://github.com/quasimatic/glance-dom',
+    download_url='https://github.com/quasimatic/glance-dom/tarball/0.1',
+    keywords=['testing', 'webdriver', 'selenium'],
+    classifiers=[],
+)
