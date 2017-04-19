8b9d07dcfe64c249a3fb89bf249cd324ff71fece
Dan Gilkerson <contact@dangilkerson.com>
2017-04-19 10:08:40 -0700

    Return empty array if no scopes are found

--- javascript/src/glance/processor.js
+++ javascript/src/glance/processor.js
@@ -1,76 +1,85 @@
 import containers from './containers';
 
 function dispatch({command, extensions, glanceDOM, result, reference}) {
-    switch (command.command) {
-        case 'beforeall':
-            extensions.getBeforeAllHooks().forEach(h => h({reference}));
-            break;
+	switch (command.command) {
+		case 'beforeall':
+			extensions.getBeforeAllHooks().forEach(h => h({reference}));
+			break;
 
-        case 'containers':
-            if (result.scopeElements) {
-                result.containerElements = containers(result.scopeElements, result.subjectElements);
-            }
+		case 'containers':
+			if (result.scopeElements) {
+				result.containerElements = containers(result.scopeElements, result.subjectElements);
+			}
 
-            if (result.subjectElements) {
-                result.scopeElements = result.subjectElements;
-            }
+			if (result.subjectElements) {
+				if (result.subjectElements.length === 0)
+					result.elementsNotFound = true;
 
-            result.targetElements = [];
-            result.subjectElements = [];
-            break;
+				result.scopeElements = result.subjectElements;
+			}
 
-        case 'intersect':
-            if (result.subjectElements.length > 0) {
-                let subjectLookup = new Set(result.subjectElements);
-                result.subjectElements = result.targetElements.filter(e => subjectLookup.has(e));
-            }
-            else {
-                result.subjectElements = result.targetElements;
-            }
+			result.targetElements = [];
+			result.subjectElements = [];
+			break;
 
-            result.targetElements = [];
-            break;
+		case 'intersect':
+			if (result.subjectElements.length > 0) {
+				let subjectLookup = new Set(result.subjectElements);
+				result.subjectElements = result.targetElements.filter(e => subjectLookup.has(e));
+			}
+			else {
+				result.subjectElements = result.targetElements;
+			}
 
-        case 'locate':
-            let locator = extensions.getLocatorForOption(command.option, command.label);
+			result.targetElements = [];
+			break;
 
-            result.targetElements = result.targetElements.concat(locator({
-                ...command,
-                extensions,
-                glanceDOM,
-                containerElements: result.containerElements
-            }));
+		case 'locate':
+			let locator = extensions.getLocatorForOption(command.option, command.label);
 
-            result.targetElements = [...new Set(result.targetElements)];
+			result.targetElements = result.targetElements.concat(locator({
+				...command,
+				extensions,
+				glanceDOM,
+				containerElements: result.containerElements
+			}));
 
-            break;
+			result.targetElements = [...new Set(result.targetElements)];
 
-        case 'filter':
-            let filter = extensions.getFilterForOption(command.option);
-            result.targetElements = filter({
-                ...command,
-                extensions,
-                elements: result.targetElements,
-                scopeElements: result.scopeElements
-            });
-            break;
+			break;
 
-        case 'afterall':
-            extensions.getAfterAllHooks().forEach(h => h({reference, elements: result.subjectElements}));
-            break;
-    }
+		case 'filter':
+			let filter = extensions.getFilterForOption(command.option);
+			result.targetElements = filter({
+				...command,
+				extensions,
+				elements: result.targetElements,
+				scopeElements: result.scopeElements
+			});
+			break;
 
-    return result;
+		case 'afterall':
+			extensions.getAfterAllHooks().forEach(h => h({reference, elements: result.subjectElements}));
+			break;
+	}
+
+	return result;
 }
 
 export default function({commands = [], extensions, glanceDOM, reference, containerElements}) {
-    let result = commands.reduce((result, command) => dispatch({
-        command,
-        extensions,
-        reference,
-        glanceDOM,
-        result
-    }), {containerElements});
+	let result = commands.reduce((result, command) => {
+			if (result.elementsNotFound) return result;
 
-    return result.subjectElements.length === 1 ? result.subjectElements[0] : result.subjectElements;
+			return dispatch({
+				command,
+				extensions,
+				reference,
+				glanceDOM,
+				result
+			});
+		},
+		{containerElements}
+	);
+
+	return result.subjectElements.length === 1 ? result.subjectElements[0] : result.subjectElements;
 };
--- javascript/test/scope-specs.js
+++ javascript/test/scope-specs.js
@@ -2,32 +2,40 @@ import dom from './dom';
 import glanceDOM from '../src/glance';
 
 describe('Scope', () => {
-    it('should narrow down to scope', () => {
-        dom.render(<div>
-            <div>
-                <div>scope</div>
-                <div id="subject">subject</div>
-            </div>
+	it('should narrow down to scope', () => {
+		dom.render(<div>
+			<div>
+				<div>scope</div>
+				<div id="subject">subject</div>
+			</div>
 
-            <div>subject</div>
-        </div>);
+			<div>subject</div>
+		</div>);
 
-        glanceDOM('scope > subject').should.deep.equal(dom.get('subject'));
-    });
+		glanceDOM('scope > subject').should.deep.equal(dom.get('subject'));
+	});
 
-    it('should limit container', () => {
-        dom.render(<div>
-            <div>
-                <div>scope-1</div>
-                <div>scope-2</div>
-                <div id="subject">subject</div>
-            </div>
-            <div>
-                <div>scope-2</div>
-                <div>subject</div>
-            </div>
-        </div>);
+	it('should limit container', () => {
+		dom.render(<div>
+			<div>
+				<div>scope-1</div>
+				<div>scope-2</div>
+				<div id="subject">subject</div>
+			</div>
+			<div>
+				<div>scope-2</div>
+				<div>subject</div>
+			</div>
+		</div>);
 
-        glanceDOM('scope-1 > scope-2 > subject').should.deep.equal(dom.get('subject'));
-    });
+		glanceDOM('scope-1 > scope-2 > subject').should.deep.equal(dom.get('subject'));
+	});
+
+	it('should not find any items if no scopes are found', () => {
+		dom.render(<div>
+			<div>subject</div>
+		</div>);
+
+		glanceDOM('scope > subject').should.deep.equal([]);
+	});
 });
