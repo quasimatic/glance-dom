5dedb35645a54f98534b04b8a2cb1ca027d47188
Jim Brännlund <jimbrannlund@fastmail.com>
2017-04-20 16:45:16 +0200

    revert name change

--- py/setup.py
+++ py/setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='glance-dom',
-    packages=['glance-dom'],
+    name='glance_dom',
+    packages=['glance_dom'],
     package_data={
-        'glance-dom': ['scr/glance-dom.js'],
+        'glance_dom': ['scr/glance-dom.js'],
     },
-    version='0.1',
+    version='0.3',
     description='A selenium automation tool that utilizes contextual labels to automate faster and loosen the ties to '
                 'the DOM.',
     author='Dan Gilkerson',
     author_email='info@quasimatic.org',
-    url='https://github.com/quasimatic/glance-dom',
-    download_url='https://github.com/quasimatic/glance-dom/tarball/0.1',
+    url='https://github.com/quasimatic/glance_dom',
+    download_url='https://github.com/quasimatic/glance_dom/tarball/0.3',
     keywords=['testing', 'webdriver', 'selenium'],
     classifiers=[],
 )
