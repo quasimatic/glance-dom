d0b76ac25d71c652f841735932c252aebfbcd6e8
Dan Gilkerson <contact@dangilkerson.com>
2017-04-23 08:06:59 -0700

    Test coverage and setting custom execute

--- javascript/src/browser/jquery-spec.js
+++ javascript/src/browser/jquery-spec.js
@@ -3,5 +3,6 @@ require('./jquery');
 describe('Glance Dom Browser Jquery', () => {
 	it('glance jquery should be global', () => {
 		window.$glance.should.not.be.null;
+		window.$glance("subject");
 	});
 });
