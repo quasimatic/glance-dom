6af78c637a641c7cd92469ad981026df11fe29a8
Dan Gilkerson <contact@dangilkerson.com>
2017-04-23 22:41:35 -0700

    Update version number for python

--- build/version.js
+++ build/version.js
@@ -4,4 +4,11 @@ var version = require('../package.json').version;
 //
 // Javascript
 //
-fs.writeFileSync("javascript/src/version.json", JSON.stringify(version), 'utf8');
+fs.writeFileSync('javascript/src/version.json', JSON.stringify(version), 'utf8');
+
+//
+// Python
+//
+var setuppy = fs.readFileSync('py/setup.py', 'utf8');
+var updatedSetupPy = setuppy.replace(/version='.*'/, 'version=\'' + version + '\'').replace(/\/tarball\/0\.3\.3/, '/tarbar/' + version);
+fs.writeFileSync('py/setup.py', updatedSetupPy, 'utf8');
