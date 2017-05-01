0217b3f64290d5c0a6a1a4cb428f34a6b3a70cb2
Dan Gilkerson <contact@dangilkerson.com>
2017-04-30 17:22:54 -0700

    Use check function for displayed filters. Add displayed-otherwise-internal.

--- javascript/src/extensions/filters/displayed.js
+++ javascript/src/extensions/filters/displayed.js
@@ -2,8 +2,9 @@ import log from '../../utils/log';
 
 function elementsFoundByDisplayProperties(elements, label, inverse) {
 	return elements.filter((e) => {
-		let displayed = e.innerText.indexOf(label) !== -1
-			|| e.value && e.value.indexOf(label) !== -1
+		let lowercasedLabel = label.toLowerCase();
+		let displayed = e.textContent.toLowerCase().indexOf(lowercasedLabel) !== -1
+			|| e.value && e.value.toLowerCase().indexOf(lowercasedLabel) !== -1
 			|| e.tagName.toLowerCase() === 'img';
 
 		return inverse ? !displayed : displayed;
@@ -13,6 +14,9 @@ function elementsFoundByDisplayProperties(elements, label, inverse) {
 export default {
 	options: {
 		'displayed': {
+			check: function({options}) {
+				return !options.includes('internal');
+			},
 			filter: function visible({elements, label}) {
 				log.debug('Filtering for elements matched with displayed properties');
 
@@ -20,10 +24,25 @@ export default {
 			}
 		},
 		'internal': {
+			check: function({options}) {
+				return !options.includes('displayed');
+			},
 			filter: function visible({elements, label}) {
 				log.debug('Filtering for elements matched with internal properties');
 
 				return elementsFoundByDisplayProperties(elements, label, true);
+			}
+		},
+		'displayed-otherwise-internal': {
+			check: function({options}) {
+				return !options.includes('displayed') && !options.includes('internal');
+			},
+			filter: function visible({elements, label}) {
+				log.debug('Filtering for elements matched with displayed properties otherwise internal ones');
+
+				let displayedElements = elementsFoundByDisplayProperties(elements, label, false);
+				log.debug('DisplayedElements:', displayedElements.length);
+				return displayedElements.length !== 0 ? displayedElements : elements;
 			}
 		}
 	}
