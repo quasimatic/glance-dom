3d695fc526d5bcd369d7bbcd2d771cea994e8b7f
Dan Gilkerson <contact@dangilkerson.com>
2017-05-11 15:09:11 -0700

    0.11.1

--- py/setup.py
+++ py/setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={
         'glance_dom': ['scr/glance-dom.js'],
     },
-    version='0.11.0',
+    version='0.11.1',
     description='A selenium automation tool that utilizes contextual labels to automate faster and loosen the ties to '
                 'the DOM.',
     author='Dan Gilkerson',
     author_email='info@quasimatic.org',
     url='https://github.com/quasimatic/glance_dom',
-    download_url='https://github.com/quasimatic/glance_dom/tarball/0.11.0',
+    download_url='https://github.com/quasimatic/glance_dom/tarball/0.11.1',
     keywords=['testing', 'webdriver', 'selenium'],
     classifiers=[],
 )
