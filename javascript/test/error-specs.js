8657b8c04b7fd188fd7384a2e06056a8866f8624
Dan Gilkerson <contact@dangilkerson.com>
2017-12-20 20:06:25 -0800

    Refactor error specs

--- javascript/test/error-specs.js
+++ javascript/test/error-specs.js
@@ -1,14 +1,7 @@
-import dom from './dom';
 import glanceDOM from '../src/glance-dom-browser';
 
 describe('Errors', () => {
-    it('should find nothing with an empty selector', () => {
-        try {
-            glanceDOM();
-            throw new Error('Didn\'t throw error');
-        }
-        catch (err) {
-            err.message.should.equal('Selector required');
-        }
-    });
+	it('should find nothing with an empty selector', () => {
+		expect(() => glanceDOM()).to.throw('Selector required');
+	});
 });
